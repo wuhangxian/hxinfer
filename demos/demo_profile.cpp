f720a51c038edfc459b3e917326b379369bb9652
wuhangxian <105161430+wuhangxian@users.noreply.github.com>
2026-08-09 02:04:40 +0800

    update demos/demo_profile.cpp

--- demos/demo_profile.cpp
+++ demos/demo_profile.cpp
@@ -8,7 +8,7 @@
 #include "base/config.h"
 #include "tensor/tensor.h"
 #include "op/math_ops.h"
-#include "model/llama_model.h"
+#include "model/causal_lm_model.h"
 #include "loader/llama7b_loader.h"
 
 using namespace hxinfer;
