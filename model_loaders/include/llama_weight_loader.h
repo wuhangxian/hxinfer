f6c82d89ed70adb5cbdf79a0be7c465e38410a94
wuhangxian <105161430+wuhangxian@users.noreply.github.com>
2026-08-09 02:04:15 +0800

    update model_loaders/include/llama_weight_loader.h

---
+++ model_loaders/include/llama_weight_loader.h
@@ -0,0 +1,23 @@
+#ifndef HXINFER_LLAMA_WEIGHT_LOADER_H
+#define HXINFER_LLAMA_WEIGHT_LOADER_H
+
+#include "model/causal_lm_model.h"
+#include "safetensors_loader.h"
+#include "base/allocator.h"
+#include <string>
+#include <memory>
+
+namespace hxinfer {
+
+class LlamaWeightLoader {
+public:
+    static std::shared_ptr<LlamaModel> load(
+        const std::string& model_dir,
+        ModelConfig& out_config,
+        const std::shared_ptr<CPUAllocator>& cpu_alloc,
+        const std::shared_ptr<CUDAAllocator>& cuda_alloc
+    );
+};
+
+} // namespace hxinfer
+#endif // HXINFER_LLAMA_WEIGHT_LOADER_H
