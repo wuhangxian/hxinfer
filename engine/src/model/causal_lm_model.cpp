b01348877a98cf648cc0e1ec4e319c8afccede47
wuhangxian <105161430+wuhangxian@users.noreply.github.com>
2026-08-09 02:03:57 +0800

    update engine/src/model/causal_lm_model.cpp

---
+++ engine/src/model/causal_lm_model.cpp
@@ -0,0 +1,13 @@
+#include "model/causal_lm_model.h"
+#include "op/math_ops.h"
+namespace hxinfer{
+    void LlamaModel::forward(std::shared_ptr<Tensor> &input, std::shared_ptr<Tensor> &output,int pos) {
+        embeddingLayer_->forward(input,ping_);
+        for(int i=0;i<blocks_.size();i++){
+            blocks_[i]->forward(ping_,pang_,pos);
+            std::swap(ping_,pang_);
+        }
+        final_norm_->forward(ping_,ping_);
+        lm_head_->forward(ping_,output);
+    }
+}
