0820971c7094283e73dda00ce169d0bd234bc5c7
wuhangxian <105161430+wuhangxian@users.noreply.github.com>
2026-08-09 11:08:37 +0800

    sync: upload missing file engine/include/layer/linear.h

---
+++ engine/include/layer/linear.h
@@ -0,0 +1,20 @@
+
+#ifndef HXINFER_LINEAR_H
+#define HXINFER_LINEAR_H
+
+#include "layer.h"
+#include "op/math_ops.h"
+namespace hxinfer{
+    class LinearLayer:public Layer{
+    private:
+        std::shared_ptr<Tensor> weight_;
+    public:
+        LinearLayer(std::shared_ptr<Tensor> weight): Layer("Linear"),
+                                                     weight_(weight){}
+        void forward(std::shared_ptr<Tensor>& input,std::shared_ptr<Tensor>& output) override{
+            matmul_tensor(input,weight_,output);
+        }
+    };
+}
+
+#endif //HXINFER_LINEAR_H
